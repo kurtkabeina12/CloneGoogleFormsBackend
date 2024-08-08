39efd9e76ff6eeddbfcbb152a3854907c53b64aa
Yana Avetisyan <yanagithuby@gmail.com>
2024-08-08 12:23:31 +0300

    update main.ts try..catch

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,27 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 
 async function bootstrap() {
- const app = await NestFactory.create(AppModule);
- app.enableCors();
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
 
- // Увеличиваем максимально допустимый размер тела запроса до 100MB
- app.use(express.json({ limit: '100mb' }));
- app.use(express.urlencoded({ limit: '100mb', extended: true }));
+    // Увеличиваем максимально допустимый размер тела запроса до 100MB
+    app.use(express.json({ limit: '100mb' }));
+    app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
- const config = new DocumentBuilder()
-    .setTitle('API документация')
-    .setDescription('Демонстрация Swagger в NestJS')
-    .setVersion('1.0')
-    .addTag('cards')
-    .build();
- const document = SwaggerModule.createDocument(app, config);
- SwaggerModule.setup('api', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('API документация')
+      .setDescription('Демонстрация Swagger в NestJS')
+      .setVersion('1.0')
+      .addTag('cards')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
- await app.listen(8888);
+    await app.listen(8888);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 bootstrap();
